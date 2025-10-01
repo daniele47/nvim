7beff4583f01b0094df87062d0105f7e7bf87e83
- <->
2025-10-02 01:07:58 +0200

    small tweaks ;

--- lsp/lua_ls.lua
+++ lsp/lua_ls.lua
@@ -68,18 +68,50 @@
 --- * [Lua.workspace.library](https://luals.github.io/wiki/settings/#workspacelibrary)
 ---
 
----@type vim.lsp.Config
 return {
-  cmd = { 'lua-language-server' },
-  filetypes = { 'lua' },
-  root_markers = {
-    '.luarc.json',
-    '.luarc.jsonc',
-    '.luacheckrc',
-    '.stylua.toml',
-    'stylua.toml',
-    'selene.toml',
-    'selene.yml',
-    '.git',
-  },
+    cmd = { 'lua-language-server' },
+    filetypes = { 'lua' },
+    root_markers = {
+        '.luarc.json',
+        '.luarc.jsonc',
+        '.luacheckrc',
+        '.stylua.toml',
+        'stylua.toml',
+        'selene.toml',
+        'selene.yml',
+        '.git',
+    },
+    settings = {
+        Lua = {
+            telemetry = { enable = false },
+            workspace = {
+                checkThirdParty = false,
+            }
+        }
+    },
+    on_init = function(client)
+        if client.workspace_folders then
+            local path = client.workspace_folders[1].name
+            if vim.uv.fs_realpath(path) == vim.fn.stdpath('config') then
+                client.config.settings = client.config.settings or {}
+                client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua or {}, {
+                    runtime = {
+                        version = 'LuaJIT',
+                        path = {
+                            'lua/?.lua',
+                            'lua/?/init.lua',
+                        },
+                    },
+                    workspace = {
+                        checkThirdParty = false,
+                        library = {
+                            vim.env.VIMRUNTIME,
+                            '${3rd}/luv/library',
+                            '${3rd}/busted/library',
+                        }
+                    },
+                })
+            end
+        end
+    end,
 }
--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -10,6 +10,15 @@ vim.api.nvim_create_autocmd("LspAttach", {
 vim.diagnostic.config({
     float = { border = "single" },
 })
+-- configure lsp floating windows
+local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
+	opts = opts or {}
+	opts.border = opts.border or "single"
+	opts.max_width = opts.max_width or 100
+	opts.max_height = opts.max_height or 15
+	return orig_util_open_floating_preview(contents, syntax, opts, ...)
+end
 
 -- enable snippets
 vim.api.nvim_create_autocmd("LspAttach", {
