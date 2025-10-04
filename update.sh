7a64452531b7ac5b518222ecbcc9693d788e8a5e
- <->
2025-10-05 00:07:20 +0200

    script variables for dir/files

--- update.sh
+++ update.sh
@@ -14,30 +14,32 @@ LSPS=(
 
 SCRIPT_PATH="$(realpath "${BASH_SOURCE[0]}")"
 SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
+LSP_DIR="$SCRIPT_DIR/lsp"
+LSP_ENABLE_FILE="$SCRIPT_DIR/plugin/02_lspenable.lua"
 TMPDIR="$(mktemp -d)"
 
 echo 'downloading lspconfig git repo...'
 git clone --depth=1 https://github.com/neovim/nvim-lspconfig/ "$TMPDIR" &>/dev/null
-rm -rf "$SCRIPT_DIR/lsp"
-mkdir -p "$SCRIPT_DIR/lsp"
+rm -rf "$LSP_DIR"
+mkdir -p "$LSP_DIR"
 for LSP in "${LSPS[@]}"; do
     echo "copying $LSP.lua into place..."
-    mv "$TMPDIR/lsp/$LSP.lua" "$SCRIPT_DIR/lsp/"
+    mv "$TMPDIR/lsp/$LSP.lua" "$LSP_DIR/"
 done
 rm -rf "$TMPDIR"
 
 echo 'updating lspenable file...'
 echo '-- enable all lsp
-local lsps_enable = {' > "$SCRIPT_DIR/plugin/02_lspenable.lua"
+local lsps_enable = {' > "$LSP_ENABLE_FILE"
 FIRST=true
 for LSP in "${LSPS[@]}"; do
     if [[ "$FIRST" == true ]]; then
-        echo -n "    " >> "$SCRIPT_DIR/plugin/02_lspenable.lua"
+        echo -n "    " >> "$LSP_ENABLE_FILE"
         FIRST=false 
     else
-        echo -n " " >> "$SCRIPT_DIR/plugin/02_lspenable.lua"
+        echo -n " " >> "$LSP_ENABLE_FILE"
     fi
-    echo -n "\"$LSP\"," >> "$SCRIPT_DIR/plugin/02_lspenable.lua"
+    echo -n "\"$LSP\"," >> "$LSP_ENABLE_FILE"
 done
 echo '
 }
