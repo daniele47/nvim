5432c1c7f90b7063aae108922d6aa35b156381a9
- <->
2025-10-05 00:20:49 +0200

    added after/lsp to allow customizing lsp configs, without changing base config

---
+++ after/lsp/lua_ls.lua
@@ -0,0 +1,35 @@
+return {
+    settings = {
+        Lua = {
+            telemetry = { enable = false },
+            workspace = {
+                checkThirdParty = false,
+            },
+        }
+    },
+    on_init = function(client)
+        if client.workspace_folders then
+            local path = client.workspace_folders[1].name
+            if vim.uv.fs_realpath(path) == vim.fn.stdpath('config') then
+                client.config.settings.Lua = {
+                    runtime = {
+                        version = 'LuaJIT',
+                        path = {
+                            'lua/?.lua',
+                            'lua/?/init.lua',
+                        },
+                    },
+                    workspace = {
+                        checkThirdParty = false,
+                        library = {
+                            vim.env.VIMRUNTIME,
+                            '${3rd}/luv/library',
+                            '${3rd}/busted/library',
+                        }
+                    },
+                }
+            end
+        end
+    end,
+}
+
